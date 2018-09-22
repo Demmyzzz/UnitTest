b2e15a1e297f204ceb7430805d50abc74c8f13c0
Demmyzzz <33146623+Demmyzzz@users.noreply.github.com>
2018-09-22 15:08:26 +0300

    Add files via upload

---
+++ test/MultTest.groovy
@@ -0,0 +1,9 @@
+class MultTest extends groovy.util.GroovyTestCase {
+    void testMultiplication() {
+        assertEquals(5, 2, 10);
+        assertEquals(2, 5, 10);
+        assertEquals(0, 0, 5);
+
+
+    }
+}
